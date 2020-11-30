9d6bd32b9c913da50550f976cd6add208338beb6
zach1590 <72477900+zach1590@users.noreply.github.com>
2020-11-29 22:05:58 -0500

    Create GPS_NEO6.h

---
+++ Include/GPS_2018-19/GPS_NEO6.h
@@ -0,0 +1,44 @@
+#ifndef ETV4_H
+#define ETV4_H
+
+#include <Arduino.h>
+#include <stdint.h>
+//From onboard-software-2018-19/src/Onboard Software V1.0/main/GPS_NEO6.h
+
+
+#include "GPS.h"
+
+class GPS_NEO6: public GPS{
+  public:
+    GPS_NEO6();
+    ~GPS_NEO6();
+  
+    void init();
+    void update();
+    
+    //getters for ETV4 specific data
+    double getLat();
+    double getLon();
+    double getAlt();
+    uint8_t getDay();
+    uint8_t getMonth();
+    uint16_t getYear();
+    uint8_t getHour();
+    uint8_t getMin();
+    uint8_t getSec();
+    double getSpeed();
+    double getCourse();
+    float getHdop();
+    uint32_t getSatellites();
+
+  protected:
+    //NEMA values
+    uint8_t day, month; 
+    uint16_t year; 
+    uint8_t hour, min, sec; 
+    double speed, course; 
+    float hdop; 
+    uint32_t satellites;
+};
+
+#endif

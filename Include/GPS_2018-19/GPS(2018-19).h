d76482a48e882ab34bbbac17fd798a53d60693e5
zach1590 <72477900+zach1590@users.noreply.github.com>
2020-11-29 22:07:49 -0500

    Create GPS(2018-19).h

---
+++ Include/GPS_2018-19/GPS(2018-19).h
@@ -0,0 +1,31 @@
+//From onboard-software-2018-19/src/Onboard Software V1.0/main/GPS.h
+//Last commit said code in broken state, dont use
+
+#ifndef GPS_H
+#define GPS_H
+
+#include <stdint.h>
+
+class GPS{
+  protected:
+    double deltaX, deltaY, Z; //change in position for each axis
+    double refLat, refLon, refAlt; //references
+
+    double lat, lon, alt;//values common to most GPS units
+    
+  public:
+    GPS();
+    ~GPS();
+    void getXYZ(float latitude, float longitude, float alt, float baseLatitude, float baseLongitude); //returns the change in X and Y position, and the current alt in meters
+    void getZero(float latitude, float longitude, float alt); //returns starting reference
+
+    virtual void init() = 0;
+    virtual void update() = 0;
+    virtual double getLat() = 0;
+    virtual double getLon() = 0;
+    virtual double getAlt() = 0;
+    virtual double getSpeed() = 0;
+    virtual uint32_t getSatellites() = 0;
+};
+
+#endif

b281c19c5363226509464c16b17de30d43681664
zach1590 <72477900+zach1590@users.noreply.github.com>
2020-11-29 22:00:47 -0500

    Update gps.hpp
    Added the stuff from data.hpp for now

--- Include/gps.hpp
+++ Include/gps.hpp
@@ -25,15 +25,79 @@ namespace aero
 {
 
 /*!
- *  \addtogroup sensor          //Change sensor to IMU?
+ *  \addtogroup sensor          //Change sensor to GPS?
  *  @{
  */
 
 //! Sensor interfaces
-namespace sensor                //Change sensor to IMU?
+namespace sensor                //Change sensor to GPS?
 {
 
+    
+/**
+ * @brief Raw GPS data
+ */
+struct __attribute__((__packed__)) GPS_t                //Should the stuff from Data.hpp be added or put somewhere else
+{
+    int32_t lat;
+    int32_t lon;
+    uint16_t speed;
+    uint8_t satellites;
+    uint16_t altitude;
+    uint32_t time;  // 4 BYTES. XX HR MIN SEC
+    uint32_t date;  // 4 BYTES. XX YR MNTH DAY
+};
 
+/**
+ * @brief Enum class for data signatures of each struct that can be sent
+ */
+enum class Signature{ Pitot, IMU, GPS, Enviro, Batt, Config, Status, Actuators, AData, Cmds, Drop };    
+
+ /**
+ * @brief Raw message struct used to represent a unparsed message
+ */
+struct __attribute__((__packed__)) RawMessage_t
+{
+    uint8_t start;       // Start byte for serial transfer
+    uint16_t link;       // Link describes the connection the message is trying to bridge. Sender --> Recipient
+    uint16_t signature;  // Bits for determining what data is being sent
+
+    uint8_t length;
+    uint8_t buffer[200]; // Actual data. Max size
+    
+
+    uint16_t crc;        // Try fast crc
+    uint8_t end;         // End byte for serial transfer
+};  
+    
+    
+ /**
+ * @brief Parsed message
+ */
+struct __attribute__((__packed__)) ParsedMessage_t
+{
+    ParsedMessage_t()
+    {
+        for(int i = 0; i < 12; ++i)
+            segments[i] = NULL;
+    } 
+
+    ~ParsedMessage_t()
+    {
+        for(int i = 0; i < 12; ++i)
+            delete segments[i]; 
+    }
+
+    ID m_from, m_to;
+    uint8_t* segments[12]; 
+    
+    GPS_t* gps( void ) const
+    {
+        int index = static_cast<int>(Signature::GPS);
+        return reinterpret_cast<GPS_t*>( segments[ index ] );
+    }
+};    
+    
 /**
  * @brief Abstract GPS definition
  */
